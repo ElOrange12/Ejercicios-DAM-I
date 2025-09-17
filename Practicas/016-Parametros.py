8946b4c04eff937d2ca71d4d1c6de53c2b171099
Dani <danieloliveiravidal12@gmail.com>
2025-09-17 11:52:42 +0200

    Create 016-Parametros.py

---
+++ Practicas/016-Parametros.py
@@ -0,0 +1,8 @@
+# Deben escribirse con camelCase
+# Deben tener un verbo (infinitivo o imperativo) y un objeto directo
+# Deben tener un nombre descriptivo
+
+def diHola():
+    print('Hola', nombre, 'yo te saludo')
+    
+diHola()
