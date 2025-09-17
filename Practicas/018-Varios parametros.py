2efb66b12d654314710b3c6f05f763da67f56ba4
Dani <danieloliveiravidal12@gmail.com>
2025-09-17 11:53:37 +0200

    Create 018-Varios parametros.py

---
+++ Practicas/018-Varios parametros.py
@@ -0,0 +1,9 @@
+# Deben escribirse con camelCase
+# Deben tener un verbo (infinitivo o imperativo) y un objeto directo
+# Deben tener un nombre descriptivo
+
+def diHola():
+    print('Hola', nombre, 'Tienes', edad, 'yo te saludo')
+    
+diHola('José Vicente', 47)
+diHola('Jorge', 48)
