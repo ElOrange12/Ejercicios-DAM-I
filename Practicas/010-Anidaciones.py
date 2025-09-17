6098a9dc9830b896171a79d37a176dc29d7e72ca
Dani <danieloliveiravidal12@gmail.com>
2025-09-17 11:45:56 +0200

    Create 010-Anidaciones.py

---
+++ Practicas/010-Anidaciones.py
@@ -0,0 +1,14 @@
+edad = int(input('Dime tu edad: '))
+# Esto no hay que hacerlo
+
+if edad < 30:
+    if edad < 20:
+        print('Eres muuuuy joven')
+    else:
+        print('Eres un joven')
+
+else:
+    if edad < 40:
+        print('Eres bastante poco joven')
+    else:
+        print('Ya no eres joven')
