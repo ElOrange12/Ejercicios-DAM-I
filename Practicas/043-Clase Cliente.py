f5b76554bb5d49aa141792a6c432338754e1eca8
Dani <danieloliveiravidal12@gmail.com>
2025-09-25 09:40:50 +0200

    Create 043-Clase Cliente.py

---
+++ Practicas/043-Clase Cliente.py
@@ -0,0 +1,30 @@
+# Poco escalable - uso de muchas variables
+
+cliente1_email = 'info@jocarsa.com'
+cliente1_direccion = 'La calle de José Vicente'
+cliente1_nombre = 'José Vicente'
+cliente1_apellidos = 'Carratala Sanchis'
+
+cliente2_email = 'info@cliente2.com'
+cliente2_direccion = 'La calle del cliente2'
+cliente2_nombre = 'Juan'
+cliente2_apellidos = 'García'
+
+class Cliente:
+    def __int__(self):
+        self.email = ''
+        self.direccion = ''
+        self.nombre = ''
+        self.apellidos = ''
+        
+cliente1 = Cliente()
+cliente1.email = 'info@jocarsa.com'
+cliente1.direccion = 'La calle de José Vicente'
+cliente1.nombre = 'José Vicente'
+cliente1.apellidos = 'Carratala Sanchis'
+
+Cliente2 = Cliente()
+cliente2.email = 'info@cliente2.com'
+cliente2.direccion = 'La calle del cliente2'
+cliente2.nombre = 'Juan'
+cliente2.apellidos = 'García'
