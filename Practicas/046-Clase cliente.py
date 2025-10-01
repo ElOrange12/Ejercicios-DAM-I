ed349629b336d761c43a198785af8259b21c71d1
Dani <danieloliveiravidal12@gmail.com>
2025-10-01 10:20:14 +0200

    Create 046-Clase cliente.py

---
+++ Practicas/046-Clase cliente.py
@@ -0,0 +1,12 @@
+# Declaramos una clase
+class Cliente():
+    def __int__(self):
+        self.email = None
+        self.nombre = None
+        self.dirección = None
+        
+# Usamos la clase instanciando en un objeto
+cliente1 = Cliente()
+cliente1.email = 'jose@empresa.com'
+cliente1.nombre = 'Jose'
+cliente1.direccion = 'La calle de Jose'
