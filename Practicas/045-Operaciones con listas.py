521ca86abd987962c164cfbb068a7e889bd3780b
Dani <danieloliveiravidal12@gmail.com>
2025-10-01 10:19:47 +0200

    Create 045-Operaciones con listas.py

---
+++ Practicas/045-Operaciones con listas.py
@@ -0,0 +1,18 @@
+cliente1 = 'Juan'
+cliente2 = 'Jorge'
+
+clientes = ['Juan', 'Jorge', 'Jaime', 'José']
+
+print(clientes)
+
+# Añadir un cliente
+
+clientes.append('Julia')
+
+print(clientes) 
+
+# Quito un elemento de la lista
+
+clientes.pop()
+
+print(clientes)
